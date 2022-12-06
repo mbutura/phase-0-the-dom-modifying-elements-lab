b2e98adc5e745a05024d359637aa118c7a4fe6d2
Alois Mbutura <alois.mbutura@student.moringaschool.com>
2022-12-06 05:34:29 +0000

    Finalize assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const mainElement = document.getElementById("main");
+mainElement.remove()
+
+const bodyElement = document.getElementsByTagName("body");
+
+const newHeader = document.createElement("h1");
+newHeader.id =  'victory';
+newHeader.textContent = "YOUR-NAME is the champion";
+bodyElement.append(newHeader);
